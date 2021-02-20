bbb16777ac44f887b3c2b3b23d9dfc514901c5e1
Praveen <guntipraveen@gmail.com>
2021-02-20 16:44:03 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome Praveen!'));
+app.get('/', (req, res) => res.send('Deepthi Praveen!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
